f86e6355dc2170e8af88d1ae2b9f381c8bd39830
liangbright <liangbright@gmail.com>
2025-04-14 15:50:23 -0400

    update

--- RotationTransform.py
+++ RotationTransform.py
@@ -23,15 +23,20 @@ class RotationTransform3D(nn.Module):
         sin_b=sin(b); cos_b=cos(b)
         sin_c=sin(c); cos_c=cos(c)
         R=torch.zeros((3,3), dtype=a.dtype, device=a.device)
-        R[0,0]=cos_c*cos_b;  R[0,1]=cos_c*sin_b*sin_a+sin_c*cos_a;  R[0,2]=-cos_c*sin_b*cos_a+sin_c*sin_a
-        R[1,0]=-sin_c*cos_b; R[1,1]=-sin_c*sin_b*sin_a+cos_c*cos_a; R[1,2]=sin_c*sin_b*cos_a+cos_c*sin_a
-        R[2,0]=sin_b;        R[2,1]=-cos_b*sin_a;                   R[2,2]=cos_b*cos_a 
+        R[0,0]=cos_b*cos_c; R[0,1]=sin_a*sin_b*cos_c-cos_a*sin_c; R[0,2]=cos_a*sin_b*cos_c+sin_a*sin_c
+        R[1,0]=cos_b*sin_c; R[1,1]=sin_a*sin_b*sin_c+cos_a*cos_c; R[1,2]=cos_a*sin_b*sin_c-sin_a*cos_c
+        R[2,0]=-sin_b;      R[2,1]=sin_a*cos_b;                   R[2,2]=cos_a*cos_b 
         return R
     
-    def forward(self, x):
+    @staticmethod
+    def rotate(x, angle):
         #x.shape (N,3)
-        R=RotationTransform3D.cal_R(self.angle)
+        R=RotationTransform3D.cal_R(angle)
         R=R.permute(1,0)#transpose
         y=matmul(x, R)
+        return y
+    
+    def forward(self, x):
+        y=RotationTransform3D.rotate(x, self.angle)
         return y
         
